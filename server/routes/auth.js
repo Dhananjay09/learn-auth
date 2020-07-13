d8e80cd717dd03c506ae8eb1afb9cfab128c22f9
Rahul Shetty <rahul.shetty@here.com>
2020-07-13 22:47:51 +0530

    Create routes for authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/signup", (__, res) => res.json("Welcome"));
+router.post("/signup", (__, res) => res.json({ welcome: "Sign up" }));
+
+router.post("/account-activation", (__, res) =>
+  res.json({ welcome: "Welcome" })
+);
+
+router.post("/signin", (__, res) => res.json({ welcome: "Sign In" }));
+
+router.post("/forgot-password", (__, res) =>
+  res.json({ welcome: "Forgot Password" })
+);
+
+router.post("/reset-password", (__, res) =>
+  res.json({ welcome: "Reset Password" })
+);
 
 module.exports = router;
