13a1fe593ffef8f79e876baebf78dbb094608fce
Rahul Shetty <rahul.shetty@here.com>
2020-07-15 21:50:23 +0530

    Update the auth models with necessary methods

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
-// const crypto = require("crypto");
+const crypto = require("crypto");
 
 const UserSchema = new mongoose.Schema(
   {
-    name: String,
-    email: String,
-    hashed_password: String,
-    salt: String,
-    role: String,
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "Subscriber",
+    },
     resetPasswordLink: {
       data: String,
       default: "",
@@ -15,5 +35,43 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.methods = {
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random() + "");
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return err;
+    }
+  },
+
+  authenticate: function (password) {
+    return this.encryptPassword(password) === this.hashed_password;
+  },
+};
+
+UserSchema.virtual("password")
+  .set(function (password) {
+    // temporary variable called password
+    this._password = password;
+
+    // generate salt and save it in our database
+    this.salt = this.makeSalt();
+
+    // encrypt the password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 module.exports = mongoose("User", UserSchema);
